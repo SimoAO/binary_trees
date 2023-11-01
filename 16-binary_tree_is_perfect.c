99f62a421ceb3c0f379a95a94b09840d7c1e9352
simoao <aitouaazizmohammed@gmail.com>
2023-11-01 22:31:18 +0100

    task18

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -46,8 +46,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 		return (0);
 
-	if (binary_tree_is_perfect(tree->left) != NULL
-			&& binary_tree_is_perfect(tree->right) != NULL)
+	if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
 		return (1);
 
 	return (0);
